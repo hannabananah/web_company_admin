9acae7f3f20962252162bd3c26147cf1a1dfefd4
heosuyeon <hsy121891@gmail.com>
2023-01-03 11:59:30 +0900

    mui styles 추가, reset css적용

--- src/screens/layout/Header.js
+++ src/screens/layout/Header.js
@@ -1,6 +1,13 @@
+import useStyles from "../../styles/Header";
+
 const Header = () => {
+  const classes = useStyles();
+  
   return (
-    <></>
+    <header className={classes.root}>
+      <h1>ANYCHAT 관리자시스템</h1>
+      <button>로그아웃</button>
+    </header>
   )
 }
 export default Header;
