90479d0306408cbe5a516ed6319fffeaf1af80f2
Hannah Kang <hanna0175@gmail.com>
2023-01-03 14:59:24 +0900

    인풋 작업중

--- src/screens/UserAccount.js
+++ src/screens/UserAccount.js
@@ -1,8 +1,46 @@
 import useStyles from "../styles/UserAccount";
 
 const UserAccount = () => {
+  const classes = useStyles();
   return (
-    <>user account</>
-  )
-}
+    <div>
+      <form className={classes.contentInput}>
+        <div className={classes.leftLayout}>
+          <label className={classes.leftText}>아이디</label>
+        </div>
+        <input
+          type="text"
+          className={classes.inputLayout}
+          name="name"
+          id="name"
+          required
+        />
+      </form>
+      <form className={classes.contentInput}>
+        <div className={classes.leftLayout}>
+          <label className={classes.leftText}>관리자 권한</label>
+        </div>
+        <input
+          type="text"
+          className={classes.inputLayout}
+          name="name"
+          id="name"
+          required
+        />
+      </form>
+      <form className={classes.contentInput}>
+        <div className={classes.leftLayout}>
+          <label className={classes.leftText}>비밀번호</label>
+        </div>
+        <input
+          type="text"
+          className={classes.inputLayout}
+          name="name"
+          id="name"
+          required
+        />
+      </form>
+    </div>
+  );
+};
 export default UserAccount;
