28f9f08676492dfc9f3f0b9cc38906e58cd1b7fb
Hannah Kang <hanna0175@gmail.com>
2023-01-04 18:11:55 +0900

    검색창 넣기

--- src/components/table/UserAccountTable.js
+++ src/components/table/UserAccountTable.js
@@ -5,7 +5,7 @@ import Pagination from "react-js-pagination";
 import useStyles from "~/styles/Table";
 const UserAccountTable = ({ changeState }) => {
   const classes = useStyles();
-  
+
   // 페이지네이션
   const [totalPage, setTotalPage] = useState(5); //임시
   const [currentPage, setCurrentPage] = useState(1);
@@ -17,109 +17,116 @@ const UserAccountTable = ({ changeState }) => {
   };
 
   return (
-    <figure className={classes.root}>
-      <button onClick={changeState}>등록</button>
-      <table className={classes.tableStyle}>
-        <thead className={classes.theadStyle}>
-          <tr>
-            <td className={classes.td} style={{ width: "100px" }}>
-              번호
-            </td>
-
-            <td
-              className={classes.td}
-              style={{
-                width: "150px",
-              }}
-            >
-              작업 날짜
-            </td>
-
-            <td className={classes.td} style={{ width: "120px" }}>
-              관리자 ID
-            </td>
-
-            <td className={classes.td} style={{ width: "120px" }}>
-              로그인 완료 시간
-            </td>
-
-            <td className={classes.td} style={{ width: "120px" }}>
-              수행 시간
-            </td>
-
-            <td className={classes.td} style={{ width: "150px" }}>
-              작업 내용
-            </td>
-          </tr>
-        </thead>
-
-        {/* <tbody>
-            {rowsData?.map((i,index) => {
-              return (
-                <tr key={index} className={classes.tableTr}>
-                  <td className={classes.tableTd}>
-                    <span className="cell">
-                      {i.comment_type == 1 ? "댓글" : "답글"}
-                    </span>
-                  </td>
-
-                  <td>
-                    
-                    <>
-                      <p className="cell">{i.profile_name}</p>
-                    </>
-                  </td>
-
-                  <td>
-                    <span className="cell" onClick={() => setTrans(!trans)}>
-                      {trans ? i.comment : i.comment_trans}
-                    </span>
-                  </td>
-
-                  <td>
-                    <span className="cell">{i.comment_count}</span>
-                  </td>
-
-                  <td>
-                    <span className="cell" style={{ textAlign: "center" }}>
-                      {`${i.reg_dt}`.slice(0, 10)}
-                      <br />
-                      {`${i.reg_dt}`.slice(11, 16)}
-                    </span>
-                  </td>
-
-                  <td>
-                    <span
-                      className="cellActionBtn"
-                      onClick={() => detailPosts(i)}
-                    >
-                      게시물 자세히 보기
-                    </span>
-                  </td>
-
-                  <td>
-                    <button
-                      className="cellActionBtn"
-                      onClick={() => deleteCommentsModal(i.cidx)}
-                    >
-                      댓글 삭제
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody> */}
-      </table>
-      <Pagination
-        activePage={currentPage}
-        totalItemsCount={postsPerPage * totalPage} // 총 포스트 갯수
-        itemsCountPerPage={postsPerPage} // 페이지당 보여줄 포스트 갯수
-        pageRangeDisplayed={10} // 페이저 갯수
-        prevPageText={"‹"}
-        nextPageText={"›"}
-        onChange={handlePageChange}
-      />
-    </figure>
+    <div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <select>
+            <option>아이디</option>
+            <option>관리자 권한</option>
+            <option>전화번호 입력</option>
+          </select>
+          <input />
+          <button>검색</button>
+        </div>
+        <button onClick={changeState}>등록</button>
+      </div>
+      <figure className={classes.root}>
+        <table className={classes.tableStyle}>
+          <thead className={classes.theadStyle}>
+            <tr>
+              <td className={classes.td} style={{ width: "100px" }}>
+                번호
+              </td>
+              <td
+                className={classes.td}
+                style={{
+                  width: "150px",
+                }}
+              >
+                작업 날짜
+              </td>
+              <td className={classes.td} style={{ width: "120px" }}>
+                관리자 ID
+              </td>
+              <td className={classes.td} style={{ width: "120px" }}>
+                로그인 완료 시간
+              </td>
+              <td className={classes.td} style={{ width: "120px" }}>
+                수행 시간
+              </td>
+              <td className={classes.td} style={{ width: "150px" }}>
+                작업 내용
+              </td>
+            </tr>
+          </thead>
+          {/* <tbody>
+              {rowsData?.map((i,index) => {
+                return (
+                  <tr key={index} className={classes.tableTr}>
+                    <td className={classes.tableTd}>
+                      <span className="cell">
+                        {i.comment_type == 1 ? "댓글" : "답글"}
+                      </span>
+                    </td>
+                    <td>
+      
+                      <>
+                        <p className="cell">{i.profile_name}</p>
+                      </>
+                    </td>
+                    <td>
+                      <span className="cell" onClick={() => setTrans(!trans)}>
+                        {trans ? i.comment : i.comment_trans}
+                      </span>
+                    </td>
+                    <td>
+                      <span className="cell">{i.comment_count}</span>
+                    </td>
+                    <td>
+                      <span className="cell" style={{ textAlign: "center" }}>
+                        {`${i.reg_dt}`.slice(0, 10)}
+                        <br />
+                        {`${i.reg_dt}`.slice(11, 16)}
+                      </span>
+                    </td>
+                    <td>
+                      <span
+                        className="cellActionBtn"
+                        onClick={() => detailPosts(i)}
+                      >
+                        게시물 자세히 보기
+                      </span>
+                    </td>
+                    <td>
+                      <button
+                        className="cellActionBtn"
+                        onClick={() => deleteCommentsModal(i.cidx)}
+                      >
+                        댓글 삭제
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody> */}
+        </table>
+        <Pagination
+          activePage={currentPage}
+          totalItemsCount={postsPerPage * totalPage} // 총 포스트 갯수
+          itemsCountPerPage={postsPerPage} // 페이지당 보여줄 포스트 갯수
+          pageRangeDisplayed={10} // 페이저 갯수
+          prevPageText={"‹"}
+          nextPageText={"›"}
+          onChange={handlePageChange}
+        />
+      </figure>
+    </div>
   );
 };
 export default UserAccountTable;
