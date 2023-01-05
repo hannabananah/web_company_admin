03d0d6c0a32e4f96af7c261f574265fd72acca9e
Hannah Kang <hanna0175@gmail.com>
2023-01-05 10:24:06 +0900

    로그인 페이지 width값 조정

--- src/styles/Login.js
+++ src/styles/Login.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles({
   container: {
-    width: "100vw",
+    width: "100%",
     height: "100vh",
     display: "flex",
     alignItems: "center",
