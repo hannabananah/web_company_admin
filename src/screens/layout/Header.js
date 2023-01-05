a890ede18c3d836b0756a8fb6feaa03c8f50f41d
heosuyeon <hsy121891@gmail.com>
2023-01-05 17:58:45 +0900

    로그아웃 버튼 이벤트

--- src/screens/layout/Header.js
+++ src/screens/layout/Header.js
@@ -6,19 +6,24 @@ const Header = () => {
   const classes = useStyles();
   const navigate = useNavigate();
   const location = useLocation();
+
   let isEmpty = false;
 
   const emptyList = ["/","/index.html"];
   emptyList.map((url) => {
     if (url == location.pathname) isEmpty = true;
   });
-  
+
+  const logOut = () => {
+    navigate('/')
+  }
+
   return (
     <>
       {allPaths.includes(window.location.pathname) && 
       <header className={classes.root}>
         <h1>ANYCHAT 관리자시스템</h1>
-        <button>로그아웃</button>
+        <button onClick={logOut}>로그아웃</button>
       </header>
       }
     </>
