db6254b960c29b7a69ea725392c47ca3ed4fc48b
heosuyeon <hsy121891@gmail.com>
2023-01-05 17:26:16 +0900

    404 페이지 뼈대 완료

--- src/assets/js/Images.js
+++ src/assets/js/Images.js
@@ -1,10 +1,12 @@
 import SVG_NAV_ICON_1ST from "../images/icon_navi_1.svg";
 import SVG_PREPARATIONS from "../images/preparations.svg";
+import SVG_NOTOUND from "../images/not_found.svg";
 
 const images = {
   icons: {
     SVG_NAV_ICON_1ST: SVG_NAV_ICON_1ST,
   },
   SVG_PREPARATIONS: SVG_PREPARATIONS,
+  SVG_NOTOUND: SVG_NOTOUND,
 };
 export default images;
--- src/styles/NotFound.js
+++ src/styles/NotFound.js
@@ -5,11 +5,31 @@ const useStyles = makeStyles({
     display: 'flex',
     flex: 1,
     // alignItems:'center'
-    paddingTop:'200px'
+    paddingTop:'50px',
+    justifyContent:'center'
   },
   contents: {
+    width:'100%',
+    maxWidth:'550px',
+  },
+  img: {
+    width:'100%'
+  },
+  text: {
+    textAlign:'center',
+    paddingTop:'50px',
+    display:'flex',
+    flexDirection:'column',
+    gap:'15px',
+    alignItems:'center',
+    fontSize:'28px',
     cursor:'default',
-    fontSize:'24px',
+  },
+  goBackBtn: {
+    padding:'10px 50px',
+    border:'none',
+    borderRadius:'10px',
+    backgroundImage: 'linear-gradient(to right, #ffd3ec, #f7d4f3, #edd5f8, #e2d7fd, #d7d9ff, #cfdeff, #c9e2ff, #c5e6ff, #c7edff, #cdf4fe, #d4fafd, #defffd)'
   }
 
 });
