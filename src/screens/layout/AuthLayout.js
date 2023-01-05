d2e5302844bd1a8c6591f22f914c1ad5afa6bd2a
heosuyeon <hsy121891@gmail.com>
2023-01-05 16:56:22 +0900

    접근제한 컴포넌트 추가

--- src/screens/layout/AuthLayout.js
+++ src/screens/layout/AuthLayout.js
@@ -1,6 +1,48 @@
+import React, { useEffect } from "react";
+import { useNavigate, useLocation, Outlet } from "react-router-dom";
+// import Cookies from "js-cookie";
+
+const path = [
+  "/index.html",
+  "/index.html/",
+  "/setting_admin/user_account",
+  "/setting_admin/user_account/",
+  "/setting_admin/my_account",
+  "/setting_admin/my_account/",
+  "/setting_admin/history",
+  "/setting_admin/history/",
+  "/member/member_status",
+  "/member/member_status/",
+  "/service",
+  "/service/",
+  "/statistics",
+  "/statistics/",
+  "/notice",
+  "/notice/",
+  "/system_settings",
+  "/system_settings/"
+];
+
 const AuthLayout = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  // let pathname = window.location.pathname;
+
+  useEffect(() => {
+    // if (!Cookies.get("id")) {
+    // if ( !localStorage.getItem("token") && path.includes(pathname) ) { // 토큰 없으면
+    //   navigate("/", { state: pathname });
+    // } 
+
+    if ( pathname == "/index.html" || pathname == "/index.html/" ) {
+      navigate("/")
+    }
+  }, []);
+
   return (
-    <></>
-  )
+    <div>
+      <Outlet />
+    </div>
+  );
 }
 export default AuthLayout;
