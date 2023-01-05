64c65ed3116d5b57744b8061848a8f9ad20c2541
heosuyeon <hsy121891@gmail.com>
2023-01-05 17:58:17 +0900

    배포환경에서 콘솔 지우기

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import reportWebVitals from './reportWebVitals';
 import './styles/reset.css';
 import { ProSidebarProvider } from 'react-pro-sidebar';
 
+// replace console.* for disable log on production
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {}
+  console.error = () => {}
+  console.debug = () => {}
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
