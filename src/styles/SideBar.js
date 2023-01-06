65d18a83030aaa9035de34366e1288ab9e53eb94
heosuyeon <hsy121891@gmail.com>
2023-01-06 20:53:04 +0900

    레이아웃 변경 됨

--- src/styles/SideBar.js
+++ src/styles/SideBar.js
@@ -1,18 +1,37 @@
 import { makeStyles } from '@mui/styles';
+import images from "~/assets/js/Images";
 
 const useStyles = makeStyles({
   root: {
-    width:'200px', 
+    width:'300px', 
     position:'sticky', 
     bottom:0, 
-    top:0 
+    top:0,
+    borderRight:'1px solid rgba(188, 191, 204,0.8)',
+    paddingTop:'40px'
   },
   container : {
-    borderRight:'1px solid #ddd', 
     height:'100%',
     display:'flex', 
     flexDirection:'column', 
   },
+  h1 : {
+    color:'#553C99',
+    fontWeight:'bold',
+    fontSize:'28px',
+    lineHeight:'28px',
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+    width:'250px',
+    margin:'0 auto',
+    cursor:'pointer',
+    "& img": {
+      width:'100%'
+    }
+  },
+
   subMenuWrap: {
     display:'flex', 
     flexDirection:'column', 
@@ -20,7 +39,7 @@ const useStyles = makeStyles({
 
   menu: {
     padding:'15px',
-    cursor:'pointer'
+    cursor:'pointer',
   },
   activeMenu: {
     padding:'15px',
@@ -37,7 +56,25 @@ const useStyles = makeStyles({
     color:'#999',
     background:'teal',
     cursor:'pointer',
+  },
+
+
+  details : {
+    "& summary" : {
+      position: 'relative',
+    },
+    "& summary::-webkit-details-marker, & summary::marker" : {
+      display: 'none',
+      content: `""`,
+    },
+    "& summary:after" : {
+      content: `" +"`,
+    },
+    "&[open] > summary:after" : {
+      content: `" -"`,
+    },
   }
-  
 });
+
+
 export default useStyles;
