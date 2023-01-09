ede01cab18b5fc8959bae0687052f6b1395d0083
heosuyeon <hsy121891@gmail.com>
2023-01-09 12:08:30 +0900

    사이드바 css 완료 (공지관리 아이콘 제외)

--- src/assets/js/Images.js
+++ src/assets/js/Images.js
@@ -3,12 +3,26 @@ import SVG_PREPARATIONS from "../images/preparations.svg";
 import SVG_NOTOUND from "../images/not_found.svg";
 import ANYCHAT_LOGO from "../images/anychat_logo.svg";
 import ANYCHAT_CI_HEADER from "../images/anychat_ci_header.svg";
+import SETTINGS_ADMIN from "../images/settings_admin.svg";
+import MANAGEMANT_MEMBER from "../images/person.svg";
+import MANAGEMANT_SERVICE from "../images/service.svg";
+import MONITORING from "../images/monitoring.svg";
+import SETTINGS from "../images/settings.svg";
+import LOGOUT from "../images/logout.svg";
+import EXPAND_MORE from "../images/expand_more.svg";
 
 const images = {
   icons: {
     SVG_NAV_ICON_1ST: SVG_NAV_ICON_1ST,
     ANYCHAT_LOGO: ANYCHAT_LOGO,
     ANYCHAT_CI_HEADER: ANYCHAT_CI_HEADER,
+    SETTINGS_ADMIN: SETTINGS_ADMIN,
+    MANAGEMANT_MEMBER: MANAGEMANT_MEMBER,
+    MANAGEMANT_SERVICE: MANAGEMANT_SERVICE,
+    MONITORING: MONITORING,
+    SETTINGS: SETTINGS,
+    LOGOUT: LOGOUT,
+    EXPAND_MORE: EXPAND_MORE,
   },
   SVG_PREPARATIONS: SVG_PREPARATIONS,
   SVG_NOTOUND: SVG_NOTOUND,
--- src/styles/SideBar.js
+++ src/styles/SideBar.js
@@ -1,80 +1,136 @@
-import { makeStyles } from '@mui/styles';
+import { makeStyles } from "@mui/styles";
+import { height } from "@mui/system";
 import images from "~/assets/js/Images";
 
 const useStyles = makeStyles({
   root: {
-    width:'300px', 
-    position:'sticky', 
-    bottom:0, 
-    top:0,
-    borderRight:'1px solid rgba(188, 191, 204,0.8)',
-    paddingTop:'40px'
+    width: "300px",
+    position: "sticky",
+    bottom: 0,
+    top: 0,
+    borderRight: "1px solid rgba(188, 191, 204,0.8)",
   },
-  container : {
-    height:'100%',
-    display:'flex', 
-    flexDirection:'column', 
+  container: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
-  h1 : {
-    color:'#553C99',
-    fontWeight:'bold',
-    fontSize:'28px',
-    lineHeight:'28px',
-    display:'flex',
-    flexDirection:'column',
-    justifyContent:'center',
-    alignItems:'center',
-    width:'250px',
-    margin:'0 auto',
-    cursor:'pointer',
+  h1: {
+    color: "#553C99",
+    fontWeight: "bold",
+    fontSize: "28px",
+    lineHeight: "28px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "250px",
+    margin: "0 auto",
+    cursor: "pointer",
+    padding: "40px 0",
     "& img": {
-      width:'100%'
-    }
+      width: "100%",
+    },
   },
 
+  // ol
   subMenuWrap: {
-    display:'flex', 
-    flexDirection:'column', 
+    display: "flex",
+    flexDirection: "column",
+    paddingBottom: "20px",
   },
 
   menu: {
-    padding:'15px',
-    cursor:'pointer',
+    position:'relative',
+    padding: "15px",
+    cursor: "pointer",
+    color: "#A0A1BF",
+    display:'flex', 
+    alignItems:'center'
   },
   activeMenu: {
-    padding:'15px',
-    background:'teal',
-    cursor:'pointer',
+    position:'relative',
+    padding: "15px",
+    background: "#EFEFFE",
+    cursor: "pointer",
+    color: "#7C4DFF",
+    fontWeight: "bold",
+    display:'flex', 
+    alignItems:'center',
+    "&:after": {
+      content: `""`,
+      width: "6px",
+      height: "100%",
+      background: "#7C4DFF",
+      position: "absolute",
+      right: "0",
+      top: "0",
+      bottom: "0",
+    },
   },
+
   subMenuList: {
-    padding:'10px 10px 10px 25px',
-    color:'#999',
-    cursor:'pointer',
+    padding: "15px 10px 15px 60px",
+    color: "#A0A1BF",
+    cursor: "pointer",
+    fontSize: "13px",
   },
   activesubMenuList: {
-    padding:'10px 10px 10px 25px',
-    color:'#999',
-    background:'teal',
-    cursor:'pointer',
+    padding: "15px 10px 15px 60px",
+    color: "#7C4DFF",
+    cursor: "pointer",
+    fontSize: "13px",
   },
 
-
-  details : {
-    "& summary" : {
-      position: 'relative',
+  details: {
+    "& summary": {
+      position: "relative",
     },
-    "& summary::-webkit-details-marker, & summary::marker" : {
-      display: 'none',
+    "& summary::-webkit-details-marker, & summary::marker": {
+      display: "none",
       content: `""`,
     },
-    "& summary:after" : {
-      content: `" +"`,
-    },
-    "&[open] > summary:after" : {
-      content: `" -"`,
-    },
-  }
-});
+    // "& summary:after" : {
+    //   content: `" +"`,
+    // },
+  },
+  iconsWrap: {
+    width:'30px', 
+    height:'30px',
+    display:'flex', 
+    justifyContent:'center', 
+    alignItems:'center',
+    marginRight:'14px',
+  },
+  iconImg: {
+    width:'20px', 
+    verticalAlign:'top'
+  },
+  arrowUp: {
+    transform:'rotateX(180deg)',
+    margin:'0 30px 0 auto',
+  },
+  arrowDown: {
+    margin:'0 30px 0 auto',
+  },
+  logoutBtn: {
+    color: "#FF314A",
+    margin: "auto 0 40px",
+    background: "transparent",
+    border: "none",
+    lineHeight: "28px",
+    padding: "18px 40px",
+    background: "#FCFCFF",
+    boxShadow: "0px 15px 18px rgba(0, 0, 0, 0.05), 0px -14px 15px #FFFFFF",
+    borderRadius: "10px",
+    alignSelf:'center',
+    display:'flex',
+    alignItems:'center',
+    gap:'15px'
+  },
 
+  /* offset-x | offset-y | blur-radius | color */
+  // box-shadow: 10px 5px 5px black;
+});
 
 export default useStyles;
