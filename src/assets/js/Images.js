54a070d765ea48544885e3831a774c1a367c977d
Hannah Kang <hanna0175@gmail.com>
2023-01-09 14:25:59 +0900

    로그인 페이지 디자인 완

--- src/assets/js/Images.js
+++ src/assets/js/Images.js
@@ -3,13 +3,21 @@ import SVG_PREPARATIONS from "../images/preparations.svg";
 import SVG_NOTOUND from "../images/not_found.svg";
 import ANYCHAT_LOGO from "../images/anychat_logo.svg";
 import ANYCHAT_CI_HEADER from "../images/anychat_ci_header.svg";
+import LOGIN_MAN from "../images/man.svg";
+import LOGIN_LOCK from "../images/lock.svg";
+import LOGIN_BG from "../images/bg1.svg";
+import LOGIN_INFO from "../images/info.svg";
 
 const images = {
   icons: {
     SVG_NAV_ICON_1ST: SVG_NAV_ICON_1ST,
     ANYCHAT_LOGO: ANYCHAT_LOGO,
     ANYCHAT_CI_HEADER: ANYCHAT_CI_HEADER,
+    LOGIN_MAN: LOGIN_MAN,
+    LOGIN_LOCK: LOGIN_LOCK,
+    LOGIN_INFO: LOGIN_INFO,
   },
+  LOGIN_BG: LOGIN_BG,
   SVG_PREPARATIONS: SVG_PREPARATIONS,
   SVG_NOTOUND: SVG_NOTOUND,
 };
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useStyles from "~/styles/Login";
+import images from "~/assets/js/Images";
 
 const Login = () => {
   const classes = useStyles();
   const [id, setId] = useState("anychat");
   const [password, setPassword] = useState("anychat21!@");
   const navigate = useNavigate();
-  const [invalid, setInvalid] = useState(false);
+  const [invaild, setInvaild] = useState(false);
 
   const onChangeId = (e) => {
     setId(e.target.value);
@@ -22,6 +23,12 @@ const Login = () => {
 
   return (
     <div className={classes.container}>
+      <img
+        className={classes.loginLogo}
+        src={images.icons.ANYCHAT_LOGO}
+        alt="애니챗 로고"
+      />
+      {/* 로그인 창 박스 */}
       <fieldset className={classes.loginContainer}>
         <div className={classes.loginTitleLayout}>
           <h2 className={classes.loginTitle}>ADMIN LOGIN</h2>
@@ -30,6 +37,11 @@ const Login = () => {
           <form onSubmit={onSubmit} className={classes.loginForm}>
             <div className={classes.formGroup}>
               <div className={classes.loginInputGroup}>
+                <img
+                  src={images.icons.LOGIN_MAN}
+                  alt="로그인 아이디 아이콘"
+                  className={classes.loginIcon1}
+                />
                 <input
                   type="text"
                   // value={id}
@@ -40,6 +52,11 @@ const Login = () => {
                 />
               </div>
               <div className={classes.loginInputGroup}>
+                <img
+                  src={images.icons.LOGIN_LOCK}
+                  alt="로그인 비밀번호 아이콘"
+                  className={classes.loginIcon2}
+                />
                 <input
                   type="password"
                   // value={password}
@@ -48,24 +65,34 @@ const Login = () => {
                   placeholder="비밀번호"
                   className={classes.loginInput}
                 />
+                {/* 로그인 오류 아이콘 */}
+                {invaild && (
+                  <img
+                    src={images.icons.LOGIN_INFO}
+                    alt="로그인 오류 아이콘"
+                    className={classes.loginErrorIcon}
+                  />
+                )}
               </div>
             </div>
+            {/* 로그인 버튼 */}
             <input type="submit" value="로그인" className={classes.loginBtn} />
           </form>
         </div>
-
-        {/* 로그인알림메시지 */}
-        {invalid && (
+        {/* 로그인 에러 알림메시지 */}
+        {invaild && (
           <div className="loginAlertMsg">
             <p>아이디와 패스워드가 일치 하지 않습니다.</p>
             <p>다른 기기에서 로그인 중입니다.</p>
           </div>
         )}
-        <div className={classes.loginText1}>
-          <p>아이디, 비밀번호 분실시 시스템 관리자에게 문의 바랍니다.</p>
-          <p className={classes.loginText2}>
-            <span>전화 : 000-0000-0000</span>
-            <span>/ Email : aaa@amam.cp.kr</span>
+        <div className={classes.loginTextStyle}>
+          <p className={classes.loginText}>
+            아이디, 비밀번호 분실시 시스템 관리자에게 문의 바랍니다.
+          </p>
+          <p className={classes.loginText}>
+            <span>전화 : 000-0000-0000 </span>
+            <span>| Email : anychat79@anychat.com</span>
           </p>
         </div>
       </fieldset>
