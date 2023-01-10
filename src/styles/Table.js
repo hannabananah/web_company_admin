3262167750220ba4fc6e629ceea04a3713c4a47d
heosuyeon <hsy121891@gmail.com>
2023-01-10 15:01:10 +0900

    테이블 스타일 전체 적용

--- src/styles/Table.js
+++ src/styles/Table.js
@@ -2,11 +2,20 @@ import { makeStyles } from "@mui/styles";
 import { COLORS } from "~/assets/colors/colors";
 
 const useStyles = makeStyles({
-  root: {},
+  root: {
+    border: `1px solid ${COLORS.STORKE.CONTENTS2}`,
+    borderRadius:'8px', 
+    boxShadow:"0px 4px 22px rgba(0, 0, 0, 0.05)"
+  },
   tableStyle: {
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderColor: COLORS.STORKE.CONTENTS2,
+    width:'100%',
+  },
+    th_td: {
+    textAlign: "center",
+    cursor: "default",
+    fontSize: "12px",
+    padding: "21px 0",
+    fontWeight: 'bold',
   },
   td: {
     textAlign: "center",
@@ -21,15 +30,3 @@ const useStyles = makeStyles({
   },
 });
 export default useStyles;
-
-
-  // tableStyle:{
-  //   width:'100%',
-  // },
-  // th_td: {
-  //   textAlign: "center",
-  //   cursor: "default",
-  //   fontSize: "12px",
-  //   padding: "21px 0",
-  //   fontWeight: 'bold',
-  // },
