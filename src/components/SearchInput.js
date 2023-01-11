649a708a9673e2d641f04807c14235f419a72402
heosuyeon <hsy121891@gmail.com>
2023-01-11 18:33:17 +0900

    버튼, input 모듈화

---
+++ src/components/SearchInput.js
@@ -0,0 +1,25 @@
+import { makeStyles } from '@mui/styles';
+// import useStyles from "~/styles/SearchInput";
+
+const SearchInput = () => {
+  const classes = useStyles();
+
+  return (
+    <input className={classes.root} />
+  )
+}
+export default SearchInput;
+
+
+const useStyles = makeStyles({
+  root: {
+    border:'1px solid #D1D1D1',
+    // lineHeight:'40px',
+    borderRadius:'8px',
+    height:'100%',
+    padding:'0 10px',
+    width:'100%',
+    maxWidth:'170px',
+    boxSizing:'border-box'
+  },
+});
