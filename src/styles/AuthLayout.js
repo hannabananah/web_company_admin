717117a9c44d82140a23f502075dfddf34980401
heosuyeon <hsy121891@gmail.com>
2023-01-11 18:32:05 +0900

    테이블 max width 전체 설정

--- src/styles/AuthLayout.js
+++ src/styles/AuthLayout.js
@@ -3,7 +3,11 @@ import { makeStyles } from '@mui/styles';
 const useStyles = makeStyles({
   outletRoot: {
     display:'flex',
-    justifyContent:'center'
+    justifyContent:'center',
+    "& > div" :{
+      width: '100%',
+      maxWidth: '1320px',
+    }
   },
 });
 export default useStyles;
