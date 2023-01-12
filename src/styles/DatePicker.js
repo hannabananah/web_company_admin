4221cd20f0275f50ce58c60aa6d0b50be37e569f
Hannah Kang <hanna0175@gmail.com>
2023-01-12 16:24:33 +0900

    app intro 공지 수정 페이지 작업중

--- src/styles/DatePicker.js
+++ src/styles/DatePicker.js
@@ -1,53 +1,56 @@
-import { makeStyles } from '@mui/styles';
+import { makeStyles } from "@mui/styles";
 import { COLORS } from "~/assets/colors/colors";
 
 export const useStyles = makeStyles({
   root: {
-    maxWidth:'190px',
-    height:'100%',
-    '& .MuiInputBase-root.MuiOutlinedInput-root': {
-      position:'relative',
-      padding:'0',
-      height:'100%',
-      borderRadius:'8px'
-    }
+    maxWidth: "190px",
+    height: "100%",
+    "& .MuiInputBase-root.MuiOutlinedInput-root": {
+      position: "relative",
+      padding: "0",
+      height: "100%",
+      borderRadius: "8px",
+    },
   },
 });
 
 export const datepickerSX = {
-  '&.MuiFormControl-root.MuiTextField-root': {
-    height:'100%',
-    background:'#fff'
+  "&.MuiFormControl-root.MuiTextField-root": {
+    height: "100%",
+    background: "#fff",
   },
-  '& .MuiInputBase-input':{
-    padding:'0',
-    cursor:'pointer',
-    width:'100%',
-    textAlign:'center',
-    color:'#D1D1D1',
-    fontSize:'14px',
-    height:'100%',
-    // background:'salmon',
+  "& .MuiInputBase-input": {
+    padding: "0",
+    cursor: "pointer",
+    width: "100%",
+    textAlign: "center",
+    color: COLORS.COLOR.BLACK,
+    fontSize: "14px",
+    height: "100%",
   },
-  '& .MuiInputAdornment-root.MuiInputAdornment-positionEnd': {
-    position:'absolute',
-    left:'0',
-    right:'0',
-    top:'0',
-    bottom:'0',
-    margin:'0',
-    height:'100%',
-    maxHeight:'100%'
+  "& .MuiInputAdornment-root.MuiInputAdornment-positionEnd": {
+    position: "absolute",
+    left: "0",
+    right: "0",
+    top: "0",
+    bottom: "0",
+    margin: "0",
+    height: "100%",
+    maxHeight: "100%",
   },
-  '& .MuiIconButton-edgeEnd.MuiIconButton-sizeMedium': {
+  "& .MuiIconButton-edgeEnd.MuiIconButton-sizeMedium": {
     // background:'teal',
-    borderRadius:'0',
-    width:'100%',
-    padding:'0',
-    height:'100%',
-    '& svg': {
-      opacity:'0',
-      visibility:'hidden'
-    }
-  }
-}
+    borderRadius: "0",
+    width: "100%",
+    padding: "0",
+    height: "100%",
+    "& svg": {
+      opacity: "0",
+      visibility: "hidden",
+    },
+  },
+  "& .css-1d3z3hw-MuiOutlinedInput-notchedOutline": {
+    border: `1px solid ${COLORS.STORKE.CONTENTS2}`,
+    borderRadius: "8px",
+  },
+};
