e62a3a75c6fc1f76e1145ae2b9e282dc9d02654f
Hannah Kang <hanna0175@gmail.com>
2023-01-12 13:55:49 +0900

    수정 데이터 파일별 name과 id명 설정

--- src/components/EditAppIntroNotice.js
+++ src/components/EditAppIntroNotice.js
@@ -7,23 +7,15 @@ const EditAppIntroNotice = ({ gobackstate, user }) => {
 
   //user info state
   const [userInfo, setUserInfo] = useState({
-    id: user.firstName,
-    auth: user.bloodGroup,
-    pwd: "",
-    chkPwd: "",
-    // phone1: user.phone1,
-    // phone2: user.phone2,
-    // phone3: user.phone3,
-    phone1: "",
-    phone2: "",
-    phone3: "",
-    email: user.email,
-    ip: user.ip,
-    use_yn: user.gender == "male" ? false : true, //male:N, female:Y
+    os: user.ip,
+    type: user.gender,
+    period: user.phone,
+    title: user.eyeColor,
+    content: user.ip,
+    desc: user.userAgent,
   });
 
-  const { auth, pwd, chkPwd, phone1, phone2, phone3, email, ip, use_yn } =
-    userInfo;
+  const { os, type, period, title, content, desc } = userInfo;
 
   const onChange = (e) => {
     console.log("onchange!!!!!!!!!!!");
@@ -34,19 +26,113 @@ const EditAppIntroNotice = ({ gobackstate, user }) => {
 
     const newInfo = {
       ...userInfo,
-      [name]: name == "use_yn" ? !userInfo.use_yn : value, //e.target의 name과 value이다.
+      [name]: name == "type" ? !userInfo.type : value, //e.target의 name과 value이다.
     };
     setUserInfo(newInfo);
   };
 
   console.log("userInfo ----------->", userInfo);
-  console.log("userInfo.use_yn ----------->", userInfo.use_yn);
+  console.log("userInfo.use_yn ----------->", userInfo.type);
 
   return (
     <figure className={classes.userAccContainer}>
       <section className={classes.titleSection}>
         <h2 className={classes.mainTitle}>App Intro 공지 수정</h2>
       </section>
+      <table className={classes.tableStyle}>
+        <colgroup>
+          <col />
+          <col />
+        </colgroup>
+        <tbody>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>OS</label>
+            </th>
+            <td className={classes.fixedLayout}>{userInfo.os}</td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>공지 유형</label>
+            </th>
+            <td className={classes.inputLayout}>
+              <input
+                value={userInfo.type}
+                onChange={onChange}
+                type="text"
+                className={classes.inputStyle}
+                name="type"
+                id="introNoti"
+                required
+              />
+            </td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>공지 노출 기간</label>
+            </th>
+            <td className={classes.inputLayout}>
+              <input
+                value={userInfo.period}
+                onChange={onChange}
+                type="text"
+                className={classes.inputStyle}
+                name="period"
+                id="introNoti"
+                required
+              />
+            </td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>공지 제목</label>
+            </th>
+            <td className={classes.inputLayout}>
+              <input
+                value={userInfo.title}
+                onChange={onChange}
+                type="text"
+                className={classes.inputStyle}
+                name="title"
+                id="introNoti"
+                required
+              />
+            </td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>공지 내용</label>
+            </th>
+            <td className={classes.inputLayout}>
+              <input
+                type="tel"
+                value={userInfo.content}
+                onChange={onChange}
+                className={classes.inputNumStyle}
+                name="content"
+                id="introNoti"
+                maxLength="3"
+                required
+              />
+            </td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>설명</label>
+            </th>
+            <td className={classes.inputLayout}>
+              <input
+                value={userInfo.desc}
+                onChange={onChange}
+                type="text"
+                className={classes.inputStyle}
+                name="desc"
+                id="introNoti"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <div className={classes.submitBtns}>
         <button onClick={gobackstate} className={classes.backBtn}>
           이전
