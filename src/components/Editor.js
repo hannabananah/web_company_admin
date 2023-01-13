d4e9abef5828de7478ad63e7d939d9a1b192fd93
Hannah Kang <hanna0175@gmail.com>
2023-01-13 10:42:35 +0900

    editor 적용중

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,21 +3,39 @@ import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import htmlToDraft from "html-to-draftjs";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+import useStyles from "~/styles/Editor";
 
 export const EditorTool = () => {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const classes = useStyles();
 
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
   const onEditorStateChange = (editorState) => {
-    // editorState에 값 설정
     setEditorState(editorState);
   };
   return (
     <>
       <Editor
-        editorState={editorState}
+        editorState={editorState} // 초기값 설정
+        onEditorStateChange={onEditorStateChange} // 에디터의 값이 변경될 때마다 onEditorStateChange 호출
         wrapperClassName="demo-wrapper"
-        editorClassName="demo-editor"
-        onEditorStateChange={onEditorStateChange}
+        editorClassName={classes.editor}
+        // 한국어 설정
+        localization={{
+          locale: "ko",
+        }}
+        placeholder="내용을 작성해주세요."
+        // 툴바 주위에 적용된 클래스
+        toolbarClassName="toolbar-class"
+        // 툴바 설정
+        toolbar={{
+          // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
+          list: { inDropdown: true },
+          textAlign: { inDropdown: true },
+          link: { inDropdown: true },
+          history: { inDropdown: false },
+        }}
       />
       <textarea
         disabled
---
+++ src/styles/Editor.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@mui/styles";
+import { COLORS } from "~/assets/colors/colors";
+
+const useStyles = makeStyles({
+  editor: {
+    height: "500px !important",
+    border: "5px solid #f1f1f1 !important",
+    padding: "5px !important",
+    borderRadius: "10px !important",
+  },
+});
+export default useStyles;
