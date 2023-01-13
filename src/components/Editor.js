f4caab8f2055a7eb97673343c3c4a232978e564c
Hannah Kang <hanna0175@gmail.com>
2023-01-13 11:49:39 +0900

    editor test

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
-import draftToHtml from "draftjs-to-html";
-import htmlToDraft from "html-to-draftjs";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 import useStyles from "~/styles/Editor";
@@ -19,27 +17,29 @@ export const EditorTool = () => {
       <Editor
         editorState={editorState} // 초기값 설정
         onEditorStateChange={onEditorStateChange} // 에디터의 값이 변경될 때마다 onEditorStateChange 호출
-        wrapperClassName="demo-wrapper"
-        editorClassName={classes.editor}
+        wrapperClassName={classes.wrapper} // 에디터와 툴바 모두에 적용되는 클래스
+        editorClassName={classes.editor} // 에디터 주변에 적용된 클래스
+        toolbarClassName="toolbar-class" // 툴바 주위에 적용된 클래스
         // 한국어 설정
-        localization={{
-          locale: "ko",
-        }}
-        placeholder="내용을 작성해주세요."
-        // 툴바 주위에 적용된 클래스
-        toolbarClassName="toolbar-class"
+        // localization={{
+        //   locale: "ko",
+        // }}
         // 툴바 설정
         toolbar={{
-          // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
-          list: { inDropdown: true },
-          textAlign: { inDropdown: true },
-          link: { inDropdown: true },
-          history: { inDropdown: false },
+          options: ["blockType", "image"],
+          blockType: {
+            inDropdown: true,
+            options: ["H1", "H2"],
+            className: "blockText",
+            dropdownClassName: "blockTextDown",
+          },
+          image: {
+            uploadCallback: EditorTool,
+            inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg",
+            alt: { present: true, mandatory: false },
+          },
         }}
-      />
-      <textarea
-        disabled
-        value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+        placeholder="내용을 작성해주세요."
       />
     </>
   );
