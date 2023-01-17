2a73d06b502976b32cb3db0bec3d3037e6cd9ad7
heosuyeon <hsy121891@gmail.com>
2023-01-17 11:43:28 +0900

    디버깅 때문에 임시로 다시 콘솔 보이게

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import './styles/reset.css';
 import { ProSidebarProvider } from 'react-pro-sidebar';
 
 // replace console.* for disable log on production
-if (process.env.NODE_ENV === 'production') {
-  console.log = () => {}
-  console.error = () => {}
-  console.debug = () => {}
-}
+// if (process.env.NODE_ENV === 'production') {
+//   console.log = () => {}
+//   console.error = () => {}
+//   console.debug = () => {}
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
