1b585fbf547cfe64bb3b32973f1ea2be36b8e64d
yhchoi <>
2023-01-18 19:00:37 +0900

    히스토리 업데이트

--- src/screens/History.js
+++ src/screens/History.js
@@ -9,6 +9,7 @@ import SelectBox from "~/components/SelectBox";
 import DatePicker from "~/components/DatePicker";
 import FilterSection from "~/components/FilterSection";
 import TableHeader from "~/components/TableHeader";
+import axios from "axios";
 
 // filter select option
 const option = [
@@ -25,33 +26,69 @@ const option = [
 const History = () => {
   const classes = useStyles();
   // 페이지네이션
+  const [totalUser, setTotalUser] = useState(0); //임시
   const [totalPage, setTotalPage] = useState(5); //임시
   const [currentPage, setCurrentPage] = useState(1);
   const postsPerPage = 10;
+
+  // 데이트픽커
+  let now = dayjs().format("YYYY-MM-DD");
+  const [start, setStart] = useState(dayjs(now));
+  const [end, setEnd] = useState(start);
+  //더미데이터
+  const [fetchData, setFetchData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [inputVal, setInputVal] = useState("");
+
+
+  useEffect(() => {
+      changePage(1);
+    setEnd(start);
+  }, [start]);
+
+  const getTotalHistry = () => {
+    axios
+        .get(`http://localhost:3001/api/access/total?s=${selectVal}&v=${inputVal}&st=${start.format("YYYY-MM-DD")}&et=${end.format("YYYY-MM-DD")}`, {
+          headers: {
+            Authorization:
+                "Bearer " +
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY1MTE5NjM1OSwiZXhwIjoxNjgyNzMyMzU5fQ.5ZxqvUdLOS8zrbCZuDqZqv4Zjox1POUrZ0Ah0u9LEbs",
+          },
+        })
+        .then(({data}) => {
+          console.log(data);
+          setTotalUser(data.cnt);
+
+        });
+  }
+
+  const changePage = (page) => {
+    getTotalHistry()
+    axios
+        .get(`http://localhost:3001/api/access/pagination?size=${postsPerPage}&page=${page}&s=${selectVal}&v=${inputVal}&st=${start.format("YYYY-MM-DD")}&et=${end.format("YYYY-MM-DD")}`, {
+          headers: {
+            Authorization:
+                "Bearer " +
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY1MTE5NjM1OSwiZXhwIjoxNjgyNzMyMzU5fQ.5ZxqvUdLOS8zrbCZuDqZqv4Zjox1POUrZ0Ah0u9LEbs",
+          },
+        })
+        .then(({data}) => {
+          console.log(data);
+          setFetchData(data);
+        }).then(setIsLoaded(true));
+  }
+
+  const handleNameChange = (e) => {
+    setInputVal(e.target.value);
+  };
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
     console.log("page  -------------------->", page);
   };
 
-  // 데이트픽커
-  let now = dayjs().format("YYYY-MM-DD");
-  const [start, setStart] = useState(dayjs(now));
-  const [end, setEnd] = useState(start);
-
   useEffect(() => {
-    setEnd(start);
-  }, [start]);
-
-  //더미데이터
-  const [fetchData, setFetchData] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    fetch("https://dummyjson.com/users/")
-      .then((res) => res.json())
-      .then((json) => setFetchData(json))
-      .then(setIsLoaded(true));
+   changePage(1);
   }, []);
 
   // 필터
@@ -72,7 +109,7 @@ const History = () => {
               onChange={onChangeSelect}
               option={option}
             />
-            <input className={classes.input} />
+            <input className={classes.input}  onChange={handleNameChange} />
           </>
         }
         right={
@@ -83,14 +120,18 @@ const History = () => {
               end={end}
               setEnd={setEnd}
             />
-            <button className={classes.searchBtn}>검색</button>
+            <button className={classes.searchBtn}
+                onClick={() => {
+                    changePage(1);
+                }}
+            >검색</button>
           </>
         }
       />
       <HistoryTable fetchData={fetchData} isLoaded={isLoaded} />
       <Pagination
         activePage={currentPage}
-        totalItemsCount={postsPerPage * totalPage} // 총 포스트 갯수
+        totalItemsCount={totalUser} // 총 포스트 갯수
         itemsCountPerPage={postsPerPage} // 페이지당 보여줄 포스트 갯수
         pageRangeDisplayed={10} // 페이저 갯수
         prevPageText={"‹"}
