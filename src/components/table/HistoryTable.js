c53be00ca5609699f2745f61dc59404aeee50750
yhchoi <>
2023-01-18 19:00:37 +0900

    히스토리 업데이트

--- src/components/table/HistoryTable.js
+++ src/components/table/HistoryTable.js
@@ -30,13 +30,13 @@ const HistoryTable = (props) => {
               관리자 ID
             </td>
 
-            <td className={classes.th_td} style={{ width: "100px" }}>
-              로그인 완료 시간
-            </td>
+            {/*<td className={classes.th_td} style={{ width: "100px" }}>*/}
+            {/*  로그인 완료 시간*/}
+            {/*</td>*/}
 
-            <td className={classes.th_td} style={{ width: "80px" }}>
-              수행 시간
-            </td>
+            {/*<td className={classes.th_td} style={{ width: "80px" }}>*/}
+            {/*  수행 시간*/}
+            {/*</td>*/}
 
             <td className={classes.th_td} style={{ width: "400px" }}>
               작업 내용
@@ -45,21 +45,21 @@ const HistoryTable = (props) => {
         </thead>
         <tbody>
           {isLoaded &&
-            fetchData?.users?.slice(0, 10).map((i, index) => {
+            fetchData?.map((i, index) => {
               return (
                 <tr key={index} className={classes.tableTr}>
                   {/* 번호 */}
-                  <td className={classes.td}>{i.id}</td>
+                  <td className={classes.td}>{i.accessKey}</td>
                   {/* 작업 날짜 */}
-                  <td className={classes.td}>{i.birthDate}</td>
+                  <td className={classes.td}>{i.createdAt}</td>
                   {/* 관리자 ID */}
-                  <td className={classes.td}>{i.maidenName}</td>
-                  {/* 로그인 완료 시간 */}
-                  <td className={classes.td}>{i.birthDate}</td>
-                  {/* 수행시간 */}
-                  <td className={classes.td}>{i.bank.cardExpire}</td>
+                  <td className={classes.td}>{i.user_id}</td>
+                  {/*/!* 로그인 완료 시간 *!/*/}
+                  {/*<td className={classes.td}>{i.birthDate}</td>*/}
+                  {/*/!* 수행시간 *!/*/}
+                  {/*<td className={classes.td}>{i.createdAt}</td>*/}
                   {/* 작업 내용 */}
-                  <td className={classes.td}>{i.university}</td>
+                  <td className={classes.td}>{i.comments}</td>
                 </tr>
               );
             })}
