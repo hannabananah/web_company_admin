97acf31428a0d78768f2ee15bfe597f2f950bec8
heosuyeon <hsy121891@gmail.com>
2023-01-18 15:54:05 +0900

    관리자 설정 - 계정 정보 수정

--- src/components/DetailAccount.js
+++ src/components/DetailAccount.js
@@ -48,101 +48,95 @@ const DetailAccount = ({ user, backState }) => {
           },
         })
         .then(({data}) => {
-          data.use_yn = data.use_yn ? true : false
-          setUserData( data);
+          // data.use_yn = data.use_yn ? true : false
+          setUserData(data);
           // setUserInfo(data);
-          console.log(user.use_yn)
+          // console.log(user.use_yn)
         });
   }, [])
 
   return (
-    <>
-      {edit ? (
-        <EditDetailAccount id={user.id} gobackstate={gobackstate} />
-      ) : (
-        <figure className={classes.userAccContainer}>
-          <table className={classes.tableStyle}>
-            <colgroup>
-              <col />
-              <col />
-            </colgroup>
-            <tbody>
-              <tr className={classes.contentInput}>
-                <th className={classes.leftLayout}>
-                  <label className={classes.leftText}>아이디</label>
-                </th>
-                <td className={classes.contentStyle}>{userData.id}</td>
-              </tr>
-              <tr className={classes.contentInput}>
-                <th className={classes.leftLayout}>
-                  <label className={classes.leftText}>관리자 권한</label>
-                </th>
-                <td className={classes.contentStyle}>{userData.grade}</td>
-              </tr>
-              <tr className={classes.contentInput}>
-                <th className={classes.leftLayout}>
-                  <label className={classes.leftText}>전화번호</label>
-                </th>
-                <td className={classes.contentStyle}>{userData.phone}</td>
-              </tr>
-              <tr className={classes.contentInput}>
-                <th className={classes.leftLayout}>
-                  <label className={classes.leftText}>이메일</label>
-                </th>
-                <td className={classes.contentStyle}>{userData.email}</td>
-              </tr>
-              <tr className={classes.contentInput}>
-                <th className={classes.leftLayout}>
-                  <label className={classes.leftText}>접속허가 IP</label>
-                </th>
-                <td className={classes.contentStyle}>{userData.allow_remote_ip}</td>
-              </tr>
-              <tr className={classes.contentInput}>
-                <th className={classes.leftLayout}>
-                  <label className={classes.leftText}>사용여부</label>
-                </th>
-                <td className={classes.contentStyle}>{userData.use_yn ? 'Y': 'N'}</td>
-              </tr>
+    <figure className={classes.userAccContainer}>
+      <table className={classes.tableStyle}>
+        <colgroup>
+          <col />
+          <col />
+        </colgroup>
+        <tbody>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>아이디</label>
+            </th>
+            <td className={classes.contentStyle}>{userData.id}</td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>관리자 권한</label>
+            </th>
+            <td className={classes.contentStyle}>{userData.grade}</td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>전화번호</label>
+            </th>
+            <td className={classes.contentStyle}>{userData.phone}</td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>이메일</label>
+            </th>
+            <td className={classes.contentStyle}>{userData.email}</td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>접속허가 IP</label>
+            </th>
+            <td className={classes.contentStyle}>{userData.allow_remote_ip}</td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>사용여부</label>
+            </th>
+            <td className={classes.contentStyle}>{userData.use_yn}</td>
+          </tr>
 
-              <tr className={classes.contentInput}>
-                <th className={classes.leftLayout}>
-                  <label className={classes.leftText}>등록일</label>
-                </th>
-                <td className={classes.contentStyle}>{dateFormat(userData.createdAt)}</td>
-              </tr>
-              <tr className={classes.contentInput}>
-                <th className={classes.leftLayout}>
-                  <label className={classes.leftText}>등록자</label>
-                </th>
-                <td className={classes.contentStyle}>{user.id}</td>
-              </tr>
-              <tr className={classes.contentInput}>
-                <th className={classes.leftLayout}>
-                  <label className={classes.leftText}>수정일</label>
-                </th>
-                <td className={classes.contentStyle}>{dateFormat(userData.updatedAt)}</td>
-              </tr>
-            </tbody>
-          </table>
-          <div className={classes.submitBtns}>
-            {/* <button onClick={backState} className={classes.backBtn}> */}
-            <button onClick={onClickPrev} className={classes.backBtn}>
-              이전
-            </button>
-            {/* <button onClick={() => onEdit(true)} className={classes.editBtn}> */}
-            <button onClick={onClickEdit} className={classes.editBtn}>
-              수정
-            </button>
-            <button onClick={openModal} className={classes.deleteBtn}>
-              삭제
-            </button>
-          </div>
-          <DeleteModal id={user.id} open={modalOpen} close={closeModal} header="계정 삭제">
-            <main>해당 계정을 삭제하시겠습니까?</main>
-          </DeleteModal>
-        </figure>
-      )}
-    </>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>등록일</label>
+            </th>
+            <td className={classes.contentStyle}>{dateFormat(userData.createdAt)}</td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>등록자</label>
+            </th>
+            <td className={classes.contentStyle}>{user.id}</td>
+          </tr>
+          <tr className={classes.contentInput}>
+            <th className={classes.leftLayout}>
+              <label className={classes.leftText}>수정일</label>
+            </th>
+            <td className={classes.contentStyle}>{dateFormat(userData.updatedAt)}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className={classes.submitBtns}>
+        {/* <button onClick={backState} className={classes.backBtn}> */}
+        <button onClick={onClickPrev} className={classes.backBtn}>
+          이전
+        </button>
+        {/* <button onClick={() => onEdit(true)} className={classes.editBtn}> */}
+        <button onClick={onClickEdit} className={classes.editBtn}>
+          수정
+        </button>
+        <button onClick={openModal} className={classes.deleteBtn}>
+          삭제
+        </button>
+      </div>
+      <DeleteModal id={user.id} open={modalOpen} close={closeModal} header="계정 삭제">
+        <main>해당 계정을 삭제하시겠습니까?</main>
+      </DeleteModal>
+    </figure>
   );
 };
 export default DetailAccount;
