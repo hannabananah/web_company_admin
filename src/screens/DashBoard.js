f89304c1b61a838c4c5f9bdcc9e518cfa6711a7b
heosuyeon <hsy121891@gmail.com>
2023-01-25 12:37:44 +0900

    대시보드 퍼블리싱 진행중

--- src/screens/DashBoard.js
+++ src/screens/DashBoard.js
@@ -5,16 +5,50 @@ const DashBoard = () => {
   const classes = useStyles();
 
   return (
-    <div>
-      <section>
+    <div className={classes.root}>
+      <section className={classes.statusSection}>
         <h2 className={classes.displayNone}>가입 및 탈퇴 현황</h2>
         <TableHeader title="가입 및 탈퇴 현황" />
+        <div className={classes.boxsWrapper}>
+          <figure className={classes.statusBoxs}>
+            <figcaption className={classes.statusLabel}>당일 기준</figcaption>
+            <div>
+              <h3>총 회원 수</h3>
+              <span></span>
+            </div>
+          </figure>
+
+          <figure className={classes.statusBoxs}>
+            <figcaption className={classes.statusLabel}>전일 기준</figcaption>
+            <div>
+              <h3>신규 가입자 수</h3>
+              <span></span>
+            </div>
+          </figure>
+
+          <figure className={classes.statusBoxs}>
+            <figcaption className={classes.statusLabel}>전일 기준</figcaption>
+            <div>
+              <h3>신규 탈퇴자 수</h3>
+              <span></span>
+            </div>
+          </figure>
+
+          <figure className={classes.statusBoxs}>
+            <figcaption className={classes.statusLabel} style={{textIndent:'-9999em'}}>전일 기준</figcaption>
+            <div>
+              <h3>휴면 회원 수</h3>
+              <span></span>
+            </div>
+          </figure>
+        </div>
       </section>
+
       <section>
         <h2 className={classes.displayNone}>그래프</h2>
       </section>
 
-      <section>
+      {/* <section>
         <h2 className={classes.displayNone}>데이터</h2>
         <div>
           <TableHeader title="데이터" />
@@ -22,7 +56,7 @@ const DashBoard = () => {
         <div>
           <TableHeader title="데이터" />
         </div>
-      </section>
+      </section> */}
     </div>
   );
 };
