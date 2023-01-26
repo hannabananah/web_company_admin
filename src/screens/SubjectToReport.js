42720f76096f4ca8989942f7a137af5378b6d387
heosuyeon <hsy121891@gmail.com>
2023-01-26 15:26:56 +0900

    테이블 컴포넌트

--- src/screens/SubjectToReport.js
+++ src/screens/SubjectToReport.js
@@ -1,8 +1,135 @@
+import { useEffect, useState } from "react";
+import useStyles from "~/styles/Add";
+import axios from "axios";
 import TableHeader from "~/components/TableHeader";
+import FilterSection from "~/components/FilterSection";
+import SelectBox from "~/components/SelectBox";
+import ReportTable from "~/components/table/ReportTable";
+import Pagination from "react-js-pagination";
+
+// filter select option
+const option = [
+  {
+    value: "Phone",
+    name: "전화번호",
+  },
+  {
+    value: "Email",
+    name: "이메일",
+  },
+];
 
 const SubjectToReport = () => {
+  const classes = useStyles();
+  const [fetchData, setFetchData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  // 필터
+  const [selectVal, setSelectVal] = useState("Phone");
+  const [inputVal, setInputVal] = useState("");
+  // 페이지네이션
+  const [totalUser, setTotalUser] = useState(0); //임시
+  const [totalPage, setTotalPage] = useState(5); //임시
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 10;
+
+  const handleNameChange = (e) => {
+    setInputVal(e.target.value);
+  };
+
+  const onChangeSelect = (event) => {
+    console.log(event.target.value);
+    setSelectVal(event.target.value);
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    // changePage(page);
+    // console.log("page  -------------------->", page);
+  };
+
+  const getTotalUserCnt = () => {
+    axios
+      .get(
+        `http://localhost:3001/api/user/totalUserCnt?s=${selectVal}&v=${inputVal}`,
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("access_token"),
+          },
+        }
+      )
+      .then(({ data }) => {
+        console.log('getTotalUserCnt________',data.userCount);
+        setTotalUser(data.userCount);
+      });
+  };
+
+  const changePage = (page) => {
+    // axios
+    //   .get(
+    //     `http://localhost:3001/api/user/pagination?size=${postsPerPage}&page=${page}&s=${selectVal}&v=${inputVal}`,
+    //     {
+    //       headers: {
+    //         Authorization: "Bearer " + localStorage.getItem("access_token"),
+    //       },
+    //     }
+    //   )
+    //   .then(({ data }) => {
+    //     console.log(data);
+    //     setFetchData(data);
+    //   })
+    //   .then(setIsLoaded(true));
+  };
+
+  useEffect(() => {
+    getTotalUserCnt();
+    changePage(1);
+  }, []);
+
   return (
-    <TableHeader title="신고 대상 회원" />
+    <div>
+      <TableHeader title="신고 대상 회원" />
+      <FilterSection
+        left={
+          <>
+            <SelectBox
+              value={selectVal}
+              onChange={onChangeSelect}
+              option={option}
+            />
+            <input
+              className={classes.filterInput}
+              onChange={handleNameChange}
+            />
+            <button
+              className={classes.searchBtn}
+              onClick={() => {
+                // changePage(1);
+              }}
+            >
+              검색
+            </button>
+            <p className={classes.memberNum}>
+            {/* 총 회원 수 :<span> {totalUser.toLocaleString()}명</span> */}
+            </p>
+          </>
+        }
+      />
+
+      <ReportTable 
+        fetchData={fetchData} 
+        isLoaded={isLoaded}
+      />
+
+      <Pagination
+        activePage={currentPage}
+        totalItemsCount={totalUser} // 총 포스트 갯수
+        itemsCountPerPage={postsPerPage} // 페이지당 보여줄 포스트 갯수
+        pageRangeDisplayed={10} // 페이저 갯수
+        prevPageText={"‹"}
+        nextPageText={"›"}
+        onChange={handlePageChange}
+      />
+    </div>
   )
 }
 export default SubjectToReport;
