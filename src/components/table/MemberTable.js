f348ce2836159711581585eec82d840ab1ed05f1
heosuyeon <hsy121891@gmail.com>
2023-01-26 15:27:38 +0900

    신고대상 회원 진행중

--- src/components/table/MemberTable.js
+++ src/components/table/MemberTable.js
@@ -6,73 +6,72 @@ const MemberTable = (props) => {
   const classes = useStyles();
 
   return (
-    <div>
-      <figure className={classes.root}>
-        <table className={classes.tableStyle}>
-          <thead className={classes.theadStyle}>
-            <tr>
-              <td className={classes.th_td} style={{ width: "40px" }}>
-                전화번호
-              </td>
-              <td
-                className={classes.th_td}
-                style={{
-                  width: "200px",
-                }}
-              >
-                계정
-              </td>
-              <td className={classes.th_td} style={{ width: "80px" }}>
-                OS
-              </td>
-              <td className={classes.th_td} style={{ width: "100px" }}>
-                생일
-              </td>
-              <td className={classes.th_td} style={{ width: "100px" }}>
-                가입일
-              </td>
-              {/*<td className={classes.th_td} style={{ width: "100px" }}>*/}
-              {/*  탈퇴일*/}
-              {/*</td>*/}
-              <td className={classes.th_td} style={{ width: "100px" }}>
-                최근 접속일
-              </td>
-            </tr>
-          </thead>
-          <tbody>
-            {isLoaded &&
-              fetchData?.map((i, index) => {
-                return (
-                  <tr key={index} className={classes.tableTr}>
-                    {/* 번호 */}
-                    <td className={classes.td}>{i.phone_no}</td>
-                    {/* 계정 */}
-                    <td className={classes.td}>{i.user_id}</td>
-                    {/* OS */}
-                    <td className={classes.td}>{i.device_type}</td>
-                    {/* 생일 */}
-                    <td className={classes.td}>{renderBirth(i.birthday)}</td>
-                    {/* 가입일 */}
-                    <td className={classes.td}>{dateFormat(i.reg_dttm)}</td>
-                    {/*/!* 탈퇴일 *!/*/}
-                    {/*<td className={classes.td}>{i.last_login_dttm}</td>*/}
-                    {/* 최근 접속일 */}
-                    <td className={classes.td}>
-                      {dateFormat(i.last_login_dttm)}
-                    </td>
-                  </tr>
-                );
-              })}
-            {/* 검색결과가 없습니다. */}
-            {/* <tr>
-            <td colSpan="8" className={classes.noDataText}>
-              검색 결과가 없습니다.
+    <figure className={classes.root}>
+      <table className={classes.tableStyle}>
+        <thead className={classes.theadStyle}>
+          <tr style={{ borderBottom: "1px solid rgba(188, 191, 204, 0.2)" }}>
+            <td className={classes.th_td} style={{ width: "40px" }}>
+              전화번호
             </td>
-          </tr> */}
-          </tbody>
-        </table>
-      </figure>
-    </div>
+            <td
+              className={classes.th_td}
+              style={{
+                width: "150px",
+
+              }}
+            >
+              계정
+            </td>
+            <td className={classes.th_td} style={{ width: "80px" }}>
+              OS
+            </td>
+            <td className={classes.th_td} style={{ width: "100px" }}>
+              생일
+            </td>
+            <td className={classes.th_td} style={{ width: "100px" }}>
+              가입일
+            </td>
+            {/*<td className={classes.th_td} style={{ width: "100px" }}>*/}
+            {/*  탈퇴일*/}
+            {/*</td>*/}
+            <td className={classes.th_td} style={{ width: "100px" }}>
+              최근 접속일
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {isLoaded &&
+            fetchData?.map((i, index) => {
+              return (
+                <tr key={index} className={classes.tableTr}>
+                  {/* 번호 */}
+                  <td className={classes.td}>{i.phone_no}</td>
+                  {/* 계정 */}
+                  <td className={classes.td}>{i.user_id}</td>
+                  {/* OS */}
+                  <td className={classes.td}>{i.device_type}</td>
+                  {/* 생일 */}
+                  <td className={classes.td}>{renderBirth(i.birthday)}</td>
+                  {/* 가입일 */}
+                  <td className={classes.td}>{dateFormat(i.reg_dttm)}</td>
+                  {/*/!* 탈퇴일 *!/*/}
+                  {/*<td className={classes.td}>{i.last_login_dttm}</td>*/}
+                  {/* 최근 접속일 */}
+                  <td className={classes.td}>
+                    {dateFormat(i.last_login_dttm)}
+                  </td>
+                </tr>
+              );
+            })}
+          {/* 검색결과가 없습니다. */}
+          {/* <tr>
+          <td colSpan="8" className={classes.noDataText}>
+            검색 결과가 없습니다.
+          </td>
+        </tr> */}
+        </tbody>
+      </table>
+    </figure>
   );
 };
 
