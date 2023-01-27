e2f3af918bcc1653c62e25ff7b32ead87bf31b8f
Hannah Kang <hanna0175@gmail.com>
2023-01-27 19:18:17 +0900

    빈 페이지에 '준비중입니다' 텍스트 넣기

--- src/screens/NotiUserEmail.js
+++ src/screens/NotiUserEmail.js
@@ -1,10 +1,12 @@
 import TableHeader from "~/components/TableHeader";
+import Preparations from "~/components/Preparations";
 
 const NoticeUserEmail = () => {
   return (
-    <div>
-      <TableHeader title="사용자 이메일 공지" /> 
-    </div>
-  )
-}
+    // <div>
+    //   <TableHeader title="사용자 이메일 공지" />
+    // </div>
+    <Preparations />
+  );
+};
 export default NoticeUserEmail;
