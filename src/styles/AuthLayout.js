4c2868068b9ad6ea91e5aa35ca1c736ede71f1a8
heosuyeon <hsy121891@gmail.com>
2023-01-27 17:29:52 +0900

    마진 left 100px추가

--- src/styles/AuthLayout.js
+++ src/styles/AuthLayout.js
@@ -3,10 +3,13 @@ import { makeStyles } from '@mui/styles';
 const useStyles = makeStyles({
   outletRoot: {
     display:'flex',
-    justifyContent:'center',
+    // justifyContent:'center',
     paddingTop:'50px',
+    marginLeft:'100px',
+    // padding:'0 100px',
     "& > div" :{
       width: '100%',
+      // margin:'0 auto',
       maxWidth: '1320px',
     }
   },
