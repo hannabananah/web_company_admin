981cd7f8e07d6b52058f7daa21aaf396235d9fe0
heosuyeon <hsy121891@gmail.com>
2023-01-27 11:34:29 +0900

    더미 이미지 삭제

--- src/components/Preparations.js
+++ src/components/Preparations.js
@@ -7,7 +7,7 @@ const Preparations = () => {
   return (
     <div className={classes.root}>
       <h3 className={classes.heading}>준비중입니다.</h3>
-      <img src={images.SVG_PREPARATIONS} className={classes.img} alt="서비스 관리 페이지, 준비중입니다."  />
+      {/* <img src={images.SVG_PREPARATIONS} className={classes.img} alt="서비스 관리 페이지, 준비중입니다."  /> */}
     </div>
   )
 }
