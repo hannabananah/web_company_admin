794310d3e051ca3f1acba90c01f3bc45ee61a113
heosuyeon <hsy121891@gmail.com>
2023-01-27 11:34:51 +0900

    더미이미지 삭제

--- src/styles/Preparations.js
+++ src/styles/Preparations.js
@@ -9,7 +9,8 @@ const useStyles = makeStyles({
     alignItems:'center',
     justifyContent:'center',
     height:'100%',
-    gap:'50px'
+    gap:'50px',
+    paddingTop:'120px'
   },
   img: {
     width:'100%'
