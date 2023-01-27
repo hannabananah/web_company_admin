d0e61d8835661ec4f598dd6bbf8fc058de226c35
heosuyeon <hsy121891@gmail.com>
2023-01-27 16:57:20 +0900

    신고 대상 회원 진행중

--- src/components/table/ReportTable.js
+++ src/components/table/ReportTable.js
@@ -8,7 +8,7 @@ const ReportTable = ({fetchData, isLoaded}) => {
   console.log('fetchData------>',fetchData)
 
   const theadData = [
-    {name:'전화번호',width:'40px',},{name:'계정',width:'150px',},{name:'OS',width:'80px',},{name:'신고 유형',width:'100px',},{name:'신고 내용',width:'100px',},{name:'신고일',width:'100px',},{name:'처리',width:'100px',},
+    {name:'번호',width:'40px',},{name:'계정',width:'150px',},{name:'OS',width:'80px',},{name:'신고 유형',width:'100px',},{name:'신고 내용',width:'100px',},{name:'신고일',width:'100px',},{name:'처리',width:'100px',},
   ];
 
   const table_header = theadData.map((item,index)=>{
@@ -32,11 +32,14 @@ const ReportTable = ({fetchData, isLoaded}) => {
         {/* 신고 유형 */}
         <td className={classes.td}>{i.warning_gbn}</td>
         {/* 신고 내용 */}
-        <td className={classes.td}>-</td>
+        <td className={classes.td}>{i.warning_gbn == '기타' ? "주식투자 권유":'-'}</td>
         {/* 신고일 */}
         <td className={classes.td}>{i.warning_dt}</td>
         {/* 처리 */}
-        <td className={classes.td}>{i.state == 0 ? '해제' : <button>계정 휴면</button>}</td>
+        <td className={classes.td}>
+          <button>계정 휴면</button>
+          {/* {i.state == 0 ? '해제' : i.state == 1 ? <button>계정 휴면</button>:'계정 휴면'} */}
+        </td>
       </tr>
     );
   })
