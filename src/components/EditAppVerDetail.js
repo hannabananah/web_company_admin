eb9622b08333b025e156f1b4dd11871e9a49c2fe
Hannah Kang <hanna0175@gmail.com>
2023-01-31 17:23:58 +0900

    App 공지 상세페이지 디자인 변경

--- src/components/EditAppVerDetail.js
+++ src/components/EditAppVerDetail.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "~/styles/Toggle.css";
-import useStyles from "~/styles/AppVerstionEdit";
+import useStyles from "~/styles/AppVersionEdit";
 import { SaveConfirmModal, UptConfirmModal } from "~/components/Modal";
 import TableHeader from "~/components/TableHeader";
 import { dateFormat, g } from "~/util/global"
