7378b0ed8a3806e2543f45fdfd96664e7ba64322
Hannah Kang <hanna0175@gmail.com>
2023-01-31 17:23:58 +0900

    App 공지 상세페이지 디자인 변경

--- src/screens/NotiAppSettingAdd.js
+++ src/screens/NotiAppSettingAdd.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 // import AppNotiAdd from "~/components/AddAppNotification";
-import useStyles from "~/styles/Add";
+import useStyles from "~/styles/AppNoticeAdd";
 import "~/styles/Toggle.css";
 import draftToHtml from "draftjs-to-html";
 import { EditorState, convertToRaw } from "draft-js";
