19fad2a51347ee6e3875a64f35105139538ab776
heosuyeon <hsy121891@gmail.com>
2023-02-02 10:19:08 +0900

    안쓰는 코드 제거

--- src/screens/layout/AuthLayout.js
+++ src/screens/layout/AuthLayout.js
@@ -1,29 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate, useLocation, Outlet } from "react-router-dom";
 // import Cookies from "js-cookie";
-
 import useStyles from "~/styles/AuthLayout";
-
-const path = [
-  "/index.html",
-  "/index.html/",
-  "/setting_admin/user_account",
-  "/setting_admin/user_account/",
-  "/setting_admin/my_account",
-  "/setting_admin/my_account/",
-  "/setting_admin/history",
-  "/setting_admin/history/",
-  "/member/member_status",
-  "/member/member_status/",
-  "/service",
-  "/service/",
-  "/statistics",
-  "/statistics/",
-  "/notice",
-  "/notice/",
-  "/system_settings",
-  "/system_settings/",
-];
 
 const AuthLayout = () => {
   const classes = useStyles();
