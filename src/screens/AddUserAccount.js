41ab7471000f06cd1444422b7fb12e2d89a0cfad
heosuyeon <hsy121891@gmail.com>
2023-02-03 16:51:12 +0900

    관리자/내 계정 정보 수정시 유효성 검사 완료

--- src/screens/AddUserAccount.js
+++ src/screens/AddUserAccount.js
@@ -6,24 +6,8 @@ import "~/styles/Toggle.css";
 import useStyles from "~/styles/AccountAdd";
 import TableHeader from "~/components/TableHeader";
 import { g } from "~/util/global";
+import { validateMsgMap } from "~/util/utils";
 import { SaveConfirmModal, UptConfirmModal } from "~/components/Modal";
-
-const validateMsgMap = {
-  id_empty: '아이디를 입력하세요.',
-  id_not_dbChek : '아이디 중복체크를 하세요.',
-  id_exists: '동일한 아이디가 존재합니다.',
-  id_valid: '사용가능한 아이디입니다.',
-  // pwd_valid: '사용가능한 비밀번호입니다.',
-  id_inValid: '아이디는 영문, 숫자 조합 4자리 이상 30자 미만이어야 합니다.',
-  grade_empty: '관리자 권한명을 입력해 주세요.',
-  pwd_empty: '비밀번호를 입력해 주세요.',
-  chkPwd_empty: '비밀번호 확인을 입력해 주세요.',
-  pwd_inValid: '비밀번호는 숫자,영문자,특수문자 조합 6자리 이상이어야 합니다.',
-  chkPwd_inValid: '비밀번호 확인을 해주세요.',
-  phone_empty: '휴대폰 번호를 입력해 주세요.',
-  email_empty: '이메일을 입력해 주세요.',
-  allow_remote_ip_empty: 'IP를 입력해 주세요.',
-}
 
 const AddUserAccount = () => {
   const classes = useStyles();
